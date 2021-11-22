1d5ad8e8a8a4b1f080ff47e20a9e025b14a7ebb0
nehaacc <yaminikamaraj0516@gmail.com>
2021-11-22 12:33:19 +0530

    testgit

---
+++ src/org/trest/Sample.java
@@ -0,0 +1,14 @@
+package org.trest;
+
+
+
+public class Sample {
+	private void clgname() {
+		System.out.println("AIHT");
+	}
+		public static void main(String[] args) {
+			Sample v=new Sample();
+			v.clgname();
+
+}
+}
